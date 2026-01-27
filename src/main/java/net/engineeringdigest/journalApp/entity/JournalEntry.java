d3292eb0555914d19db392501ae74b7429579f61
Abhishek Kumar <abhisheksdot@gmail.com>
2026-01-27 15:40:40 +0530

    Merge remote-tracking branch 'origin/secondry' into secondry

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,6 +1,9 @@
 package net.engineeringdigest.journalApp.entity;
 
+import com.sun.istack.internal.NotNull;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,9 +13,11 @@ import java.util.Date;
 
 @Document (collection = "journal_entries")
 @Data
+@NoArgsConstructor
 public class JournalEntry {
     @Id
     private ObjectId id;
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime date;
